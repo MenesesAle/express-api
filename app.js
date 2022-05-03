62e15ceb6d630389e3fd56c3ac4ddf699c652cbb
MenesesAle <developermeneses@gmail.com>
2022-05-03 15:36:57 -0500

    Configurando express, y agregando Endpoint GET

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+const app = express()
+app.use(express.json()) // indicamos que usaremos JSON
+
+const port = 3000
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+app.get('/v1/explorers', (req, res) => {
+  console.log(`Api Explorers GET ALL request ${new Date()}`)
+  const explorer1 = {id: 1, name: "Ale1"}
+  const explorer2 = {id: 2, name: "Ale2"}
+  const explorer3 = {id: 3, name: "Ale3"}
+  const explorer4 = {id: 4, name: "Ale4"}
+  const explorers = [explorer1, explorer2, explorer3, explorer4]
+  res.status(200).json(explorers)
+})
